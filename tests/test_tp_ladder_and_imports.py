7e8974696311dd4767243d80c5d23d1e87bc7322
vahidshirdel2021 <vahid.shirdel.2021@gmail.com>
2026-09-06 20:51:32 +0330

    Delete tests directory

--- tests/test_tp_ladder_and_imports.py
+++
@@ -1,7 +0,0 @@
-def test_core_imports():
-    import strategy
-    import backtest
-    from signal_engine.confluence.layer import generate_trade_signals
-    assert callable(strategy.get_signal_with_reason)
-    assert callable(backtest.run_backtest)
-    assert callable(generate_trade_signals)
